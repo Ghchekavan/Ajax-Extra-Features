3a36ed1e20b88153991e23f6470645ea8336e686
Ghchekavan <98634151+Ghchekavan@users.noreply.github.com>
2022-05-03 14:29:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,15 +39,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('❤️‍🔥𝖆𝖉𝖉 𝖒𝖊 𝖙𝖔 𝖞𝖔𝖚𝖗 𝖌𝖗𝖔𝖚𝖕❤️‍🔥', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🦋𝖈𝖍𝖆𝖓𝖓𝖊𝖑🦋', url='https://t.me/KL_MOVIES_MAIN_GROUP'),
+            InlineKeyboardButton('🦋𝖌𝖗𝖔𝖚𝖕🦋', url='https://t.me/KL_MOVIES_MAIN_GROUP')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('🐬𝖍𝖊𝖑𝖕🐬', callback_data='help'),
+            InlineKeyboardButton('🐬𝖆𝖇𝖔𝖚𝖙🐬', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('❤️‍🔥𝖏𝖔𝖎𝖓 𝖒𝖞 𝖌𝖗𝖔𝖚𝖕❤️‍🔥', url='https://t.me/KL_MOVIES_MAIN_GROUP')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
@@ -82,15 +82,15 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('❤️‍🔥𝖆𝖉𝖉 𝖒𝖊 𝖙𝖔 𝖞𝖔𝖚𝖗 𝖌𝖗𝖔𝖚𝖕❤️‍🔥', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🦋𝖈𝖍𝖆𝖓𝖓𝖊𝖑🦋', url='https://t.me/KL_MOVIES_MAIN_GROUP'),
+            InlineKeyboardButton('🦋𝖌𝖗𝖔𝖚𝖕🦋', url='https://t.me/KL_MOVIES_MAIN_GROUP')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('🐬𝖍𝖊𝖑𝖕🐬', callback_data='help'),
+            InlineKeyboardButton('🐬𝖆𝖇𝖔𝖚𝖙🐬', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('❤️‍🔥𝖏𝖔𝖎𝖓 𝖒𝖞 𝖌𝖗𝖔𝖚𝖕❤️‍🔥', url='https://t.me/KL_MOVIES_MAIN_GROUP')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
